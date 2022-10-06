3696eb934233f46dd5e59ddf8b28813e9310204d
Ionut Boghean <johnny.boghean.developer@gmail.com>
2022-10-06 07:27:05 +0100

    good progress on section express + typescript integration

--- projects/server/build/index.js
+++ projects/server/build/index.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log('hi there');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// importing npm modules
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cookie_session_1 = __importDefault(require("cookie-session"));
+// importing routers
+const loginRoutes_1 = require("./routes/loginRoutes");
+// ---
+const app = (0, express_1.default)();
+// ---
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use((0, cookie_session_1.default)({ keys: ['string'] }));
+app.use(loginRoutes_1.router);
+// ---
+app.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
--- projects/server/src/index.ts
+++ projects/server/src/index.ts
@@ -1,1 +1,22 @@
-console.log('hi there');
+// importing npm modules
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+// importing routers
+import { router } from './routes/loginRoutes';
+
+// ---
+
+const app = express();
+
+// ---
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['string'] }));
+app.use(router);
+
+// ---
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
